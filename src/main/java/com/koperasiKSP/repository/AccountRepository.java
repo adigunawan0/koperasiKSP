9e1dd62f7ccf1fa3f814e3f669a94bc0aa7ebac6
Adi Gunawan <gunawan.adi37@gmail.com>
2022-09-30 10:31:30 +0700

    perbaikan query

--- src/main/java/com/koperasiKSP/repository/AccountRepository.java
+++ src/main/java/com/koperasiKSP/repository/AccountRepository.java
@@ -8,16 +8,16 @@ import org.springframework.data.repository.query.Param;
 public interface AccountRepository extends JpaRepository<Account, String> {
 
     @Query("""
-            SELECT COUNT(*)
-            FROM Account
-            WHERE Account.account = :email
+            SELECT COUNT(ac)
+            FROM Account AS ac
+            WHERE ac.email = :email
             """)
     Long countByEmail(@Param("email") String email);
 
     @Query("""
             SELECT ac
             FROM Account AS ac
-            WHERE ac.nama = :nama
+            WHERE ac.name = :nama
             """)
     Account findByName(@Param("nama") String nama);
 }
